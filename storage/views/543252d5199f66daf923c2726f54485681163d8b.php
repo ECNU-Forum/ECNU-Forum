1268197e85f367c0bb630c9fd03f9bc4cfb20de7
Mercurio <10175101282@stu.ecnu.edu.cn>
2022-03-18 20:02:45 +0800

    Update forum-zh-Hans.js

---
+++ storage/views/543252d5199f66daf923c2726f54485681163d8b.php
@@ -0,0 +1,8 @@
+<?php echo $translator->trans('fof-byobu.email.body.private_discussion_created', [
+    '{recipient_display_name}' => $user->display_name,
+    '{actor_display_name}' => $blueprint->discussion->user->display_name,
+    '{discussion_title}' => $blueprint->discussion->title,
+    '{discussion_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->discussion->id]),
+]); ?>
+
+<?php /**PATH /var/www/flarum/vendor/fof/byobu/resources/views/emails/privateDiscussionCreated.blade.php ENDPATH**/ ?>
